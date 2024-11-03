7c2c660b7b313a64e44e6413e8fd93d361e3c1dc
block_137 <1347584512@qq.com>
2024-11-03 21:49:13 +0800

    把renderLevel的Injection改成无须捕捉Local变量

--- common/src/main/java/net/depression/mixin/client/LevelRendererMixin.java
+++ common/src/main/java/net/depression/mixin/client/LevelRendererMixin.java
@@ -4,10 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.depression.client.ClientPTSDManager;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.core.BlockPos;
@@ -32,18 +29,23 @@ public class LevelRendererMixin {
 
     @Shadow @Final private EntityRenderDispatcher entityRenderDispatcher;
 
-    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;entitiesForRendering()Ljava/lang/Iterable;"),
-            locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    private void renderFalseEntities(PoseStack poseStack, float f, long var3, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci, ProfilerFiller profilerFiller, Vec3 vec3, double d, double e, double g, Matrix4f matrix4f2, boolean bl2, Frustum frustum, float h, boolean bl3, boolean bl4, MultiBufferSource.BufferSource bufferSource) {
+    @Shadow @Final private RenderBuffers renderBuffers;
+
+    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;entitiesForRendering()Ljava/lang/Iterable;"))
+    private void renderFalseEntities(PoseStack poseStack, float f, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci) {
         if (level == null) {
             return;
         }
         String dimensionType = level.dimension().location().toString();
         if (ClientPTSDManager.falseEntities.containsKey(dimensionType)) {
+            Vec3 vec3 = camera.getPosition();
+            double d = vec3.x();
+            double e = vec3.y();
+            double g = vec3.z();
             ClientPTSDManager.falseEntities.get(dimensionType).forEach(pair -> {
                 Entity entity = pair.getFirst();
                 BlockPos blockPos = entity.blockPosition();
-                entityRenderDispatcher.render(entity, entity.getX() - d, entity.getY() - e, entity.getZ() - g, entity.getYRot(), 0, poseStack, bufferSource,
+                entityRenderDispatcher.render(entity, entity.getX() - d, entity.getY() - e, entity.getZ() - g, entity.getYRot(), 0, poseStack, renderBuffers.bufferSource(),
                         LightTexture.pack(entity.isOnFire() ? 15 : level.getBrightness(LightLayer.BLOCK, blockPos), level.getBrightness(LightLayer.SKY, blockPos)));
             });
         }
